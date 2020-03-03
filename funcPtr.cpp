d346093cd7776ffee9e9697f620b82e2ff6c3b88
Hyhlík, Tomáš, Bc <hyhlito1@fel.cvut.cz>
2020-03-03 01:23:39 +0100

    tweaked funcPtr example

--- funcPtr.cpp
+++ funcPtr.cpp
@@ -2,67 +2,71 @@
 #include <string>
 
 
-///////////////////////////////
 typedef void (*fun2)();
 typedef fun2 (*fun1)(int *, bool *);
 
+//////////////////////////////////////////////////////
 class StateMachine 
 {
-
-    bool success;
     fun1 fp;
 
 public:
     StateMachine()
     {
-        fp = (fun1) state_01;
-        success = false;
+        fp = (fun1) state_01;   // starting state
     }
 
-    //////////////////////////////////////////////////////
+    void start()
+    {
+        for (int i = 0; i < 6; i++) {
+            rxHandler(&i);
+        }
+    }
+    void rxHandler(int *val) 
+    {
+        bool success = false;
+
+        if (fp != nullptr) {
+            fp = (fun1) fp(val, &success);
+            if (!success) {
+                std::cout << "Failed to handle received command: " 
+                    <<  *val << "\n";
+                fp = nullptr;
+            }
+        }
+    }
+
+    //////////////////////////// DEFINED STATES
     static fun2 state_04(int *cmd, bool *ok) 
     {
             std::cout << "State \t04\tcmd: " << *cmd << "\n"; 
+            *ok = true;
             return (fun2)nullptr; 
     }
 
     static fun2 state_03(int *cmd, bool *ok) 
     {
             std::cout << "State \t03\tcmd: " << *cmd << "\n"; 
+            *ok = true;
             return (fun2)state_04; 
     }
 
     static fun2 state_02(int *cmd, bool *ok) 
     {
             std::cout << "State \t02\tcmd: " << *cmd << "\n"; 
+            *ok = true;
             return (fun2)state_03; 
     }
 
     static fun2 state_01(int *cmd, bool *ok) 
     {
             std::cout << "State \t01\tcmd: " << *cmd << "\n"; 
+            *ok = true;
             return (fun2)state_02; 
-    }
-
-
-    //////////////////////////////////////////////////////
-    void rxHandler(int *val) 
-    {
-            if (fp != nullptr) {
-                fp = (fun1) fp(val, &success);
-            }
-    }
-    //////////////////////////////////////////////////////
-    void start()
-    {
-
-        for (int i = 0; i < 6; i++) {
-            rxHandler(&i);
-        }
     }
 };
 
-//////////////////////////////////////////////////////
+////////////////////////////////////////////////////// MAIN
 int main()
 {
     std::cout << "AppStart..............." << std::endl;
